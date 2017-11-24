7d6e0433c92731c1ec01c473394901b0d03329d6
Jason Neal <jason.neal@astro.up.pt>
2017-11-24 21:20:27 +0000

    Fix up tests to passing.

--- tests/test_IPconvolution.py
+++ tests/test_IPconvolution.py
@@ -1,4 +1,5 @@
-
+from hypothesis import given
+import hypothesis.strategies as st
 import numpy as np
 import pytest
 
@@ -10,18 +11,16 @@ from convolve_spectrum.IP_multi_Convolution import ip_convolution as ip_multi_Co
 from convolve_spectrum.IP_Convolution import IPconvolution 
 from convolve_spectrum.IP_multi_Convolution import IPconvolution as IPmulticonvolution
 
-def test_wav_selector():
-    x = [1, 2, 3, 4, 6]
-    y = [1, 2, 1, 1, 2]
-    x_np = np.array(x)
-    y_np = np.array(y)
-    assert isinstance(wav_selector(x, y, 1, 3), list)
-    assert isinstance(wav_selector(x_np, y_np, 1, 3), list)
-    assert isinstance(wav_selector(x, y, 1, 3)[0], list)
-    assert isinstance(wav_selector(x, y, 1, 3)[1], list)
-    assert isinstance(wav_selector(x_np, y_np, 1, 3)[0], np.ndarray)
-    assert isinstance(wav_selector(x_np, y_np, 1, 3)[1], np.ndarray)
-    assert wav_selector(x, y, 0, 3)[0] == [1, 2]
+@given(st.lists(st.floats()), st.floats(), st.floats())
+def test_wav_selector(wav, wav_min, wav_max):
+    y = np.copy(wav)
+    wav2, y2 = wav_selector(wav, y, wav_min, wav_max)
+    
+    assert isinstance(wav2, np.ndarray)
+    assert isinstance(y2, np.ndarray)
+    assert all(wav2 >= wav_min)
+    assert all(wav2 <= wav_max)
+    assert len(wav2) == len(y2)
 
 
 def test_fast_convolution():
@@ -36,20 +35,21 @@ def test_fast_convolution():
         assert fast_convolve(a_val, R, b, c, 5) == 1     # Test a flat input of 1s gives a flat ouput of 1s
         assert fast_convolve(a_val, R, b, 0 * c, 5) == 0     # Test a flat input of 1s gives a flat ouput of 1s
 
-
-def test_IPconvolution():
-    wave = [1, 2, 3, 5, 6, 7, 8, 9, 10, 11]
-    flux = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+# TODO: A result that is not just ones.
+def test_ip_convolution():
+    wave = [1, 2, 3, 5, 6, 7, 10, 11]
+    flux = [1, 1, 1, 1, 1, 1, 1, 1]
     chip_limits = [2, 9]
     R = 100
-    ans = IPconvolution(wave, flux, chip_limits, R, plot=False)
-    assert ans == [1, 1]
+    new_wav, new_flux = ip_convolution(wave, flux, chip_limits, R, plot=False)
+    assert np.all(new_flux == [1, 1, 1, 1, 1])
+    assert np.all(new_wav == [2, 3, 5, 6, 7])
 
 
 def test_ip_wrapper():
     a = np.linspace(2130, 2170, 1024)
     b = np.linspace(2100, 2200, 1024)
-    assert np.all(IPconvolution(a, b, [2140, 2165], R=50000, plot=False),
+    assert np.allclose(IPconvolution(a, b, [2140, 2165], R=50000, plot=False),
                   ip_convolution(a, b, [2140, 2165], R=50000, plot=False))
 
 
