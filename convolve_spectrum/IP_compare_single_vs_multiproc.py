3d2662a88d6fb36b36beca801647f5b1a47b11fd
Jason Neal <jason.neal@astro.up.pt>
2018-02-19 22:41:02 +0000

    Remove duplicate codes from compare_sinlge_multiprocess convolution code.
    Import all from other functions.

--- convolve_spectrum/IP_compare_single_vs_multiproc.py
+++ convolve_spectrum/IP_compare_single_vs_multiproc.py
@@ -11,176 +11,11 @@ from __future__ import division, print_function
 from datetime import datetime as dt
 
 import matplotlib.pyplot as plt
-import multiprocess as mprocess
 import numpy as np
-from tqdm import tqdm
 
-
-def wav_selector(wav, flux, wav_min, wav_max):
-    """Wavelength selector.
-
-    If passed lists it will return lists.
-    If passed np arrays it will return arrays
-
-    """
-    wav = np.asarray(wav)
-    flux = np.asarray(flux)
-    # Super Fast masking with numpy
-    mask = (wav > wav_min) & (wav < wav_max)
-    wav_sel = wav[mask]
-    flux_sel = flux[mask]
-    return [wav_sel, flux_sel]
-
-
-def unitary_Gauss(x, center, fwhm):
-    """Gaussian_function of area=1.
-
-    p[0] = A;
-    p[1] = mean;
-    p[2] = fwhm;
-    """
-    sigma = np.abs(fwhm) / (2 * np.sqrt(2 * np.log(2)))
-    Amp = 1.0 / (sigma * np.sqrt(2 * np.pi))
-    tau = -((x - center) ** 2) / (2 * (sigma ** 2))
-    result = Amp * np.exp(tau)
-
-    return result
-
-
-def fast_convolve(wav_val, R, wav_extended, flux_extended, fwhm_lim):
-    """IP convolution multiplication step for a single wavelength value."""
-    fwhm = wav_val / R
-    # Mask of wavelength range within 5 fwhm of wav
-    index_mask = ((wav_extended > (wav_val - fwhm_lim * fwhm)) &
-                  (wav_extended < (wav_val + fwhm_lim * fwhm)))
-
-    flux_2convolve = flux_extended[index_mask]
-    # Gaussian Instrument Profile for given resolution and wavelength
-    IP = unitary_Gauss(wav_extended[index_mask], wav_val, fwhm)
-
-    sum_val = np.sum(IP * flux_2convolve)
-    # Correct for the effect of convolution with non-equidistant positions
-    unitary_val = np.sum(IP * np.ones_like(flux_2convolve))
-
-    return sum_val / unitary_val
-
-
-def wrapper_fast_convolve(args):
-    """Wrapper for fast_convolve needed to unpack the arguments for.
-
-    fast_convolve as multiprocess.Pool.map does not accept multiple
-    arguments.
-    """
-    return fast_convolve(*args)
-
-
-def multi_ip_convolution(wav, flux, chip_limits, R, fwhm_lim=5.0, plot=True,
-                         verbose=True, numProcs=None):
-    """Spectral convolution which allows non-equidistant step values."""
-    timeInit = dt.now()
-
-    # Make sure they are numpy arrays
-    wav = np.asarray(wav, dtype='float64')
-    flux = np.asarray(flux, dtype='float64')
-
-    wav_chip, flux_chip = wav_selector(wav, flux, chip_limits[0],
-                                       chip_limits[1])
-    # We need to calculate the fwhm at this value in order to set the starting
-    # point for the convolution
-    fwhm_min = wav_chip[0] / R  # fwhm at the extremes of vector
-    fwhm_max = wav_chip[-1] / R
-
-    # Wide wavelength bin for the resolution_convolution
-    wav_min = wav_chip[0] - fwhm_lim * fwhm_min
-    wav_max = wav_chip[-1] + fwhm_lim * fwhm_max
-    wav_ext, flux_ext = wav_selector(wav, flux, wav_min, wav_max)
-
-    print("Starting the Resolution convolution...")
-
-    # multiprocessing part
-    if numProcs is None:
-        numProcs = mprocess.cpu_count() - 1
-
-    mprocPool = mprocess.Pool(processes=numProcs)
-
-    args_generator = tqdm([[wav, R, wav_ext, flux_ext, fwhm_lim]
-                           for wav in wav_chip])
-
-    flux_conv_res = np.array(mprocPool.map(wrapper_fast_convolve,
-                                           args_generator))
-
-    mprocPool.close()
-    timeEnd = dt.now()
-    print("Multi-Proc convolution has been completed in "
-          "{} using {}/{} cores.\n".format(timeEnd - timeInit, numProcs,
-                                           mprocess.cpu_count()))
-
-    if plot:
-        plt.figure(1)
-        plt.xlabel(r"wavelength [ nm ])")
-        plt.ylabel(r"flux [counts] ")
-        plt.plot(wav_chip, flux_chip / np.max(flux_chip), color='k',
-                 linestyle="-", label="Original spectra")
-        plt.plot(wav_chip, flux_conv_res / np.max(flux_conv_res), 'ro',
-                 label="Spectrum observed at R={0}.".format(R))
-        plt.legend(loc='best')
-        plt.title(r"Convolution by an Instrument Profile ")
-        plt.show()
-
-    return [wav_chip, flux_conv_res]
-
-
-def single_ip_convolution(wav, flux, chip_limits, R, fwhm_lim=5.0,
-                          plot=True, verbose=True):
-    """Spectral convolution which allows non-equidistant step values."""
-    timeInit = dt.now()
-
-    # Make sure they are numpy arrays
-    wav = np.asarray(wav, dtype='float64')
-    flux = np.asarray(flux, dtype='float64')
-
-    wav_chip, flux_chip = wav_selector(wav, flux, chip_limits[0],
-                                       chip_limits[1])
-    # We need to calculate the fwhm at this value in order to set the starting
-    # point for the convolution
-    fwhm_min = wav_chip[0] / R  # fwhm at the extremes of vector
-    fwhm_max = wav_chip[-1] / R
-
-    # Wide wavelength bin for the resolution_convolution
-    wav_min = wav_chip[0] - fwhm_lim * fwhm_min
-    wav_max = wav_chip[-1] + fwhm_lim * fwhm_max
-    wav_ext, flux_ext = wav_selector(wav, flux, wav_min, wav_max)
-
-    print("Starting the Resolution convolution...")
-    # Predefine array space
-    flux_conv_res = np.empty_like(wav_chip, dtype="float64")
-    counter = 0
-    base_val = len(wav_chip) // 20  # Adjust here to change % between reports
-
-    for n, wav in enumerate(wav_chip):
-        # Put convolution value directly into the array
-        flux_conv_res[n] = fast_convolve(wav, R, wav_ext, flux_ext, fwhm_lim)
-        if (n % base_val == 0) and verbose:
-            counter = counter + 5  # And adjust here to change % between reports
-            print("Resolution Convolution at {0}%%...".format(counter))
-
-    timeEnd = dt.now()
-    print("Single-Proc convolution has been completed in"
-          " {}.\n".format(timeEnd - timeInit))
-
-    if plot:
-        plt.figure(1)
-        plt.xlabel(r"wavelength [ nm ])")
-        plt.ylabel(r"flux [counts] ")
-        plt.plot(wav_chip, flux_chip / np.max(flux_chip), color='k',
-                 linestyle="-", label="Original spectra")
-        plt.plot(wav_chip, flux_conv_res / np.max(flux_conv_res), 'ro',
-                 label="Spectrum observed at R={0}.".format(R))
-        plt.legend(loc='best')
-        plt.title(r"Convolution by an Instrument Profile ")
-        plt.show()
-    return [wav_chip, flux_conv_res]
-
+from convolve_spectrum.IP_Convolution import ip_convolution as single_ip_convolution
+from convolve_spectrum.IP_Convolution import unitary_Gauss
+from convolve_spectrum.IP_multi_Convolution import ip_convolution as multi_ip_convolution
 
 if __name__ == "__main__":
     # Example usage of this convolution
@@ -191,13 +26,20 @@ if __name__ == "__main__":
     # range in which to have the convolved values. Be careful of the edges!
     chip_limits = [2042, 2049]
     R = 2000
+
+    time_init = dt.now()
     single_convolved_wav, single_convolved_flux = single_ip_convolution(wav, flux, chip_limits, R,
                                                                         fwhm_lim=5.0, plot=False,
                                                                         verbose=True)
+    time_end = dt.now()
 
     multi_convolved_wav, multi_convolved_flux = multi_ip_convolution(wav, flux, chip_limits, R,
                                                                      fwhm_lim=5.0, plot=False,
                                                                      verbose=True)
+    time_end_multi = dt.now()
+
+    print("Time for normal convolution {}".format(time_end - time_init))
+    print("Time from multiprocess convolution {}".format(time_end_multi - time_end))
 
     plt.figure()
     plt.plot(single_convolved_wav, single_convolved_flux, 'ro', label='single')
